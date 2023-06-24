e99877bae4120c42642ba0c04ae3b4a605d9941d
Marian <neboduus@gmail.com>
2023-06-24 15:27:58 +0200

    Fix function and param names

--- etl/lib.py
+++ etl/lib.py
@@ -13,13 +13,13 @@ def read_data(data: str, sep: str = ',') -> pd.DataFrame:
     return pd.read_csv(StringIO(data), sep=sep)
 
 
-def parse_birthday(birthday: str, date_format: str = "%Y-%m-%d") -> Union[datetime, NaTType]:
+def parse_birthdate(birthdate: str, date_format: str = "%Y-%m-%d") -> Union[datetime, NaTType]:
     try:
-        return datetime.strptime(birthday, date_format)
+        return datetime.strptime(birthdate, date_format)
     except (Exception,):
         return pd.NaT
 
 
 def convert_birthdate(employee_df: pd.DataFrame) -> pd.DataFrame:
-    employee_df['ParsedBirthDate'] = employee_df['BirthDate'].apply(parse_birthday)
+    employee_df['ParsedBirthDate'] = employee_df['BirthDate'].apply(parse_birthdate)
     return employee_df
