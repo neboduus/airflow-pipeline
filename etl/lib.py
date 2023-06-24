f720e57131dd80e3825aed304c35f02543b1da6d
Marian <neboduus@gmail.com>
2023-06-24 14:44:32 +0200

    Implement test for convert_birthdate fun

--- etl/lib.py
+++ etl/lib.py
@@ -1,10 +1,24 @@
 import logging
+from datetime import datetime
 from io import StringIO
+from typing import Union
 
 import pandas as pd
+from pandas._libs import NaTType
 
 logger = logging.getLogger(__name__)
 
 
-def read_data(data: str, sep: str = ','):
+def read_data(data: str, sep: str = ',') -> pd.DataFrame:
     return pd.read_csv(StringIO(data), sep=sep)
+
+
+def parse_birthday(birthday: str, date_format: str = "%Y-%m-%d") -> Union[datetime, NaTType]:
+    try:
+        return datetime.strptime(birthday, date_format)
+    except (Exception,):
+        return pd.NaT
+
+
+def convert_birthdate(employee_df: pd.DataFrame) -> pd.DataFrame:
+    pass
--- etl/test_lib.py
+++ etl/test_lib.py
@@ -1,11 +1,17 @@
+import json
 import logging
 import os
 from pathlib import Path
 
-from etl.lib import read_data
+from etl.lib import read_data, convert_birthdate
 
 logger = logging.getLogger(__name__)
 current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+
+
+def get_test_resource(filename: str) -> dict:
+    with open(current_path / 'test_resources' / filename) as file:
+        return json.load(file)
 
 
 def test_read_data():
@@ -18,3 +24,13 @@ def get_employee_data():
     with open(f"{current_path}/data/employee_data.csv") as csv_file:
         employee_data = ''.join(csv_file.readlines())
     return employee_data
+
+
+def test_birthdate_conversion():
+    employee_data = get_employee_data()
+    employee_df = read_data(employee_data)
+    employee_df = convert_birthdate(employee_df)
+    expected_test_resource = "expected_birthdate_column_after_conversion.json"
+    expected_birthdate_col_json = get_test_resource(expected_test_resource)
+    parsed_birthdate_col_json = json.loads(employee_df["ParsedBirthDate"].to_json())
+    assert expected_birthdate_col_json == parsed_birthdate_col_json
