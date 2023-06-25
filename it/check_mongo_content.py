7773ca57d873fdc155672d8283907d69a5563515
Marian <neboduus@gmail.com>
2023-06-25 03:35:21 +0200

    Assert also length of results in Integration Test

--- it/check_mongo_content.py
+++ it/check_mongo_content.py
@@ -16,7 +16,9 @@ if __name__ == '__main__':
     mongo_db_client = pymongo.MongoClient("mongodb://mongouser:mongouserpass@mongodb:27017/")
     mongo_db = mongo_db_client["db"]
     etl_pipeline_results = mongo_db["etl_pipeline_results"]
-    data = [x for x in etl_pipeline_results.find({})][0]
+    data = [x for x in etl_pipeline_results.find({})]
+    assert len(data) != 0, "Mongo does not contain expected Data. IT FAILED."
+    data = data[0]
     del data["_id"]
     expected_data = read_json_as_dict('expected_mongo_content.json')
     assert data == expected_data, "Mongo does not contain expected Data. IT FAILED."
